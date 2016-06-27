f7048baf0476979c83eddeb6954782d5594608bc
Kousuke Tada <ktada@comp.is.uec.ac.jp>
2016-06-27 10:22:52 +0900

    some fix

--- .emacs.el
+++ .emacs.el
@@ -1,3 +1,6 @@
+;; load-path
+(setq load-path (cons (expand-file-name "~/.emacs.d/") load-path))
+
 ;; 日本語設定
 (set-locale-environment nil)
 (set-language-environment "Japanese")
@@ -18,14 +21,20 @@
 ;; scroll
 (setq scroll-conservatively 1)
 
+;; vhdl-mode
+(setq auto-mode-alist (cons '("\\.vhd?\\'" . vhdl-mode) auto-mode-alist))
+
 ;; cursor
 (column-number-mode 1)
 (line-number-mode 1)
 (column-number-mode t)
 (show-paren-mode 1)
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
 (global-hl-line-mode t)
 (global-linum-mode t)
-(setq linum-format "%4d||")
+(setq linum-format "%04d||")
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -48,4 +57,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass))))
+
+
