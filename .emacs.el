c43e4e0cb73e374a7da38b6fe408639741d2cc1f
Kousuke Tada <pekaso34@gmail.com>
2017-02-02 11:22:22 +0900

    気まぐれに色とかをいじった

--- .emacs.el
+++ .emacs.el
@@ -134,7 +134,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (auto-complete))))
+ '(package-selected-packages (quote (auto-complete)))
+ '(safe-local-variable-values (quote ((TeX-master . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
